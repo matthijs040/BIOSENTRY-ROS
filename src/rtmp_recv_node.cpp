76cb052ed6f216c9ec7d5a3daffa1491a210c7d8
Matthijs Besselink <matthijsbesselink@live.nl>
2020-11-21 16:35:29 +0100

    improved protocol parse.

--- src/rtmp_recv_node.cpp
+++ src/rtmp_recv_node.cpp
@@ -2,25 +2,35 @@
 
 #include "opencv2/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
+#include "cv_bridge/cv_bridge.h"
 
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "rtmp_recv_node");
-    auto n = ros::NodeHandle();
-    std::string URL = "";
-    URL = std::string(argv[1]);
-
-    ROS_INFO("Got the URL: %s", URL.c_str() );
-    
-    // Got an RTMP URL. Attempt to parse.
-    if(URL.substr(0,3) == "rtmp://")
+    if(argc == 2 )
     {
-        cv::VideoCapture cap = cv::VideoCapture(URL);
-        cv::Mat frame = cv::Mat();
-        while(cap.read(frame))
+        ros::init(argc, argv, "rtmp_recv_node");
+        auto n = ros::NodeHandle();
+        std::string URL = "";
+        URL = std::string(argv[1]);
+
+        ROS_INFO("Got the URL: %s", URL.c_str() );
+        std::string protocol = URL.substr(0, URL.find("://"));
+
+        // Got an RTMP URL. Attempt to parse.
+        if(protocol == "rtmp")
         {
-            std::cout << "I have a frame of width: " << frame.cols << " height: " << frame.rows << "\n";
+            ros::Publisher pub = n.advertise<sensor_msgs::Image>("biosentry/image_raw", 1, true);
+            cv::VideoCapture cap = cv::VideoCapture(URL);
+
+            cv::Mat frame = cv::Mat();
+            auto imsg = cv_bridge::CvImage();
+            imsg.encoding = "bgr8";
+            
+            while(cap.read(frame))
+            {
+                imsg.image = frame;
+                pub.publish(imsg.toImageMsg());
+            }
         }
     }
 
